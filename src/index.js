b20798eabaca6ad9045d6b4a46531e7bfcd89fc9
Aleazar yilma <yilma4060@gmail.com>
2022-08-24 20:16:19 +0300

    Add instant score update on create

--- src/index.js
+++ src/index.js
@@ -4,19 +4,27 @@ import Score from './modules/score.js';
 const apiCalls = require('./modules/apiCall.js');
 const updateDom = require('./modules/updateDom.js');
 
+// api url
 const baseurl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games';
 const relativeurl = '/bmlDurY1wb3kTftOecfQ/scores';
+
+// html element
 const refresh = document.querySelector('button');
 const submit = document.querySelector('#submit');
+
+// on page load
 updateDom(baseurl, relativeurl);
 
+// on refresh
 refresh.addEventListener('click', () => {
   updateDom(baseurl, relativeurl);
 });
-submit.addEventListener('click', () => {
+
+// on submit
+submit.addEventListener('click', async () => {
   const user = document.querySelector('#name').value;
   const scoreValue = document.querySelector('#score').value;
   const userScore = new Score(user, scoreValue);
-  apiCalls.setScores(baseurl, relativeurl, JSON.stringify(userScore));
+  await apiCalls.setScores(baseurl, relativeurl, JSON.stringify(userScore));
   updateDom(baseurl, relativeurl);
 });
